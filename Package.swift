9487cfef4e7bfaf184278f908dae3eda0f7b7b9d
Dardo Matias Mansilla <dardo.mansilla@fluxit.com.ar>
2021-12-15 21:18:17 -0300

    removed process

--- Package.swift
+++ Package.swift
@@ -22,10 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "UIPackage",
-            dependencies: [],
-            resources: [
-                .process("Views/TestLogin")
-            ]),
+            dependencies: []),
         .testTarget(
             name: "UIPackageTests",
             dependencies: ["UIPackage"]),
